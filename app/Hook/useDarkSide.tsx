bf3911b93f129a5d4336e45343dcc09a44b51cac
jamitz440 <jamster44085@gmail.com>
2023-10-13 21:39:34 +0100

    Storage fix

--- app/Hook/useDarkSide.tsx
+++ app/Hook/useDarkSide.tsx
@@ -1,16 +1,25 @@
 import { useEffect, useState } from "react";
 
 export default function useDarkSide() {
-  const [theme, setTheme] = useState(localStorage.theme);
+  // Start with an undefined theme and update once we can access localStorage
+  const [theme, setTheme] = useState<string | undefined>();
+
+  useEffect(() => {
+    // This code runs after component mounts on the client side
+    setTheme(localStorage.getItem("theme") || "light");
+  }, []);
+
   const colorTheme = theme === "dark" ? "light" : "dark";
 
   useEffect(() => {
-    const root = window.document.documentElement;
-    root.classList.remove(colorTheme);
-    root.classList.add(theme);
+    if (theme) {
+      const root = window.document.documentElement;
+      root.classList.remove(colorTheme);
+      root.classList.add(theme);
 
-    // save theme to local storage
-    localStorage.setItem("theme", theme);
+      // save theme to local storage
+      localStorage.setItem("theme", theme);
+    }
   }, [theme, colorTheme]);
 
   return [colorTheme, setTheme];
