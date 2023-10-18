12e0886f5521445cfd95ca0acdecf6b497067b0a
jamitz440 <113603804+jamitz440@users.noreply.github.com>
2023-10-18 11:59:10 +0100

    Update page.tsx
    Updating the description

--- app/page.tsx
+++ app/page.tsx
@@ -106,16 +106,14 @@ const EmailButton: FC<ButtonProps> = ({ children, text, link }) => {
 
 const InfoBox = () => {
   return (
-    <div className="md:mx-12 md:my-8 mx-8 my-8">
+    <div className="md:mx-12 md:my-8 mx-8 my-4">
       <h1 className="font-bold text-lg flex-wrap text-text-900">About</h1>
       <div className="flex-wrap flex h-[150px] sm:h-[200px] max-w-3xl md:max-w-6xl text-text ">
-        <p className="sm:line-clamp-none line-clamp-5 overflow-auto text-xs sm:text-base md:text-lg">
+        <p className="overflow-auto text-xs sm:text-base md:text-lg">
           I am an A Tech (Av) by trade with a passion for coding. I got into web
           development because I love the flexibility it offers – there&apos;s
           always more than one way to solve a problem or create something cool.
-          My experience includes developing engineering programs for aircraft,
-          even though I haven&apos;t put any of that work out in the public
-          domain. I tackle challenges head-on, believing there&apos;s a solution
+          My experience includes developing engineering programs for aircraft and some sideprojects in django and react.  I tackle challenges head-on, believing there&apos;s a solution
           to every problem. When I&apos;m not coding, I&apos;m usually gaming or
           experimenting with my 3D printer.
         </p>
